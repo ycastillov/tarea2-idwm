21d97affcb2cd55cff8903ab696622ec06cc6c31
Yamir <yamcast12@gmail.com>
2024-12-07 19:51:49 -0300

    build: add and install flowbite and tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-idwm',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tarea2-idwm';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
